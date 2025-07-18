149fc3f843ef6e33aeaac716fce90bf9d7c26acb
Vinícius <viniciussrdev@hotmail.com>
2025-07-18 12:14:50 -0300

    chore: commit inicial, setup do projeto

---
+++ src/main/java/dev/viniciussr/gamerental/GameRentalApiApplication.java
@@ -0,0 +1,13 @@
+package dev.viniciussr.gamerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRentalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameRentalApiApplication.class, args);
+    }
+
+}
