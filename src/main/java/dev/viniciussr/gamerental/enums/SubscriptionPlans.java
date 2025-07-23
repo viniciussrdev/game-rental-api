f35b8e940b8865f37e0a0a38a03e780ba7125bde
Vinícius <viniciussrdev@hotmail.com>
2025-07-22 23:10:20 -0300

    docs: melhorar comentários em SubscriptionPlans

--- src/main/java/dev/viniciussr/gamerental/enums/SubscriptionPlans.java
+++ src/main/java/dev/viniciussr/gamerental/enums/SubscriptionPlans.java
@@ -2,7 +2,7 @@ package dev.viniciussr.gamerental.enums;
 
 // Enum: Planos de Assinatura
 public enum SubscriptionPlans {
-    NOOB, // Direito ao aluguel de 1 jogo por vez
-    PRO, // Direito ao aluguel de 3 jogos simultaneamente
-    LEGEND // Direito ao aluguel de 5 jogos simultaneamente
+    NOOB, // Confere direito ao aluguel de 1 jogo por vez
+    PRO, // Confere direito ao aluguel de 3 jogos simultaneamente
+    LEGEND // Confere direito ao aluguel de 5 jogos simultaneamente
 }
