edbdef65e381b3d7fd4cd0fe72c5f901950da0b7
Vinícius <viniciussrdev@hotmail.com>
2025-08-01 00:46:34 -0300

    fix: corrigir nomes de variáveis e remover campos desnecessários

--- src/main/java/dev/viniciussr/gamerental/dto/UserRegisterDto.java
+++ src/main/java/dev/viniciussr/gamerental/dto/UserRegisterDto.java
@@ -1,12 +1,9 @@
 package dev.viniciussr.gamerental.dto;
 
 import dev.viniciussr.gamerental.enums.SubscriptionPlans;
-import dev.viniciussr.gamerental.enums.UserRole;
 import jakarta.validation.constraints.*;
 
 public record UserRegisterDto(
-
-        Long idUser,
 
         @NotBlank(message = "Nome do usuário é campo obrigatório")
         String name,
@@ -19,13 +16,7 @@ public record UserRegisterDto(
         @Size(min = 6, max = 20, message = "A senha deve ter entre 6 e 20 caracteres")
         String password,
 
-        @NotNull(message = "Tipo de usuário é campo obrigatório")
-        UserRole role,
-
         @NotNull(message = "Plano de assinatura é campo obrigatório")
-        SubscriptionPlans plan,
-
-        @Min(value = 0, message = "Quantidade mínima de aluguéis ativos: 0 (zero)")
-        Integer activeRentals
+        SubscriptionPlans plan
 ) {
 }
--- src/main/java/dev/viniciussr/gamerental/service/AuthorizationService.java
+++ src/main/java/dev/viniciussr/gamerental/service/AuthorizationService.java
@@ -16,8 +16,8 @@ public class AuthorizationService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
     }
 }
