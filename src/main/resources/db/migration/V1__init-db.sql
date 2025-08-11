6c174eefc75aec329a4b849d5a0f5b809766da4c
Vinícius <viniciussrdev@hotmail.com>
2025-08-11 20:12:03 -0300

    refactor: alterar tamanho de campos

--- src/main/resources/db/migration/V1__init-db.sql
+++ src/main/resources/db/migration/V1__init-db.sql
@@ -2,8 +2,8 @@
 CREATE TABLE tb_user (
     id_user BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(20) NOT NULL,
     role ENUM('ADMIN', 'USER') NOT NULL,
     plan ENUM('NOOB', 'PRO', 'LEGEND') NOT NULL,
     active_rentals INT NOT NULL DEFAULT 0
